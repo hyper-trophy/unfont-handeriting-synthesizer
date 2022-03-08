ee36004feca65477d629747f1b981c3db62af219
Harsh Suthar <bernaullis@gmail.com>
2022-03-09 00:43:52 +0530

    frontend made

--- Public/script.js
+++ Public/script.js
@@ -1,8 +1,16 @@
-let canvas = document.createElement('canvas')
+let canvas = document.querySelector('#mainCanvas')
 let ctx = canvas.getContext('2d')
-
-let backgroundImageUrl =  'https://images.unsplash.com/photo-1586075010923-2dd4570fb338?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFwZXIlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+let divExpanded = false
+let backgroundImageUrl = 'https://images.unsplash.com/photo-1586075010923-2dd4570fb338?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFwZXIlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
 let backgroundImageDataUri
+let delta = 4
+let svg = document.querySelector('#canvas')
+svg.style.height = '110px'
+svg.style.width = '1100px'
+let inputBox = document.querySelector('#text-input')
+let submitBtn = document.querySelector('#draw-button')
+let img = document.createElement('img');
+// document.querySelector('body').append(canvas)
 
 function urlContentToDataUri(url) {
     return fetch(url)
@@ -39,13 +47,6 @@ const initCanvas = async () => {
     return;
 }
 
-let svg = document.querySelector('#canvas')
-
-let inputBox = document.querySelector('#text-input')
-let submitBtn = document.querySelector('#draw-button')
-
-
-let img = document.createElement('img');
 
 
 let getLines = (text) => {
@@ -72,7 +73,7 @@ const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 const drawLineOnCanvas = (src, i) => {
     return new Promise((resolve, reject) => {
         img.onload = () => {
-            ctx.drawImage(img, 150, 420, 880, 100, 100, 100 + 40 * i, 880, 100);
+            ctx.drawImage(img, 120, 50, 1400, 110, 0, 100 + 40 * i, 1080, 110);
             resolve()
         }
         img.onerror = reject
@@ -81,6 +82,8 @@ const drawLineOnCanvas = (src, i) => {
 }
 
 let writeLines = async (text) => {
+    // canvas.style.display = "block"
+    // console.log(canvas.style.display)
     await initBackgroundImageDataUri()
     await initCanvas()
     const lines = getLines(text)
@@ -90,8 +93,8 @@ let writeLines = async (text) => {
         var clickEvent = document.createEvent('MouseEvents');
         clickEvent.initEvent('mousedown', true, true);
         submitBtn.dispatchEvent(clickEvent);
-        await wait(8000)
-        svg.innerHTML = svg.innerHTML.replaceAll('stroke: black', 'stroke: #240d94')
+        await wait(delta*1000)
+        svg.innerHTML = svg.innerHTML.replaceAll('stroke: black', 'stroke: #1a0064') //#1a0064
         var xml = new XMLSerializer().serializeToString(svg);
         var svg64 = btoa(xml);
         var b64Start = 'data:image/svg+xml;base64,';
@@ -106,6 +109,39 @@ let writeLines = async (text) => {
     downloadPageAndClearCanvas()
 }
 
-text = ``
+function toogleTextArea() {
+    let textInput = document.querySelector('#textInput')
+    let bottomDiv = document.querySelector('.bottom')
+    if (!divExpanded) {
+        textInput.style.width = 'calc(100% - 2rem)'
+        textInput.style.marginRight = '1rem'
+        textInput.style.marginBottom = '0'
+        bottomDiv.style.flexDirection = 'column'
+        bottomDiv.style.alignItems = 'center'
+        bottomDiv.style.height = 'calc(100% - 7rem)'
+        divExpanded = true
+    } else {
+        textInput.style.width = '100%'
+        textInput.style.marginRight = '0'
+        textInput.style.marginBottom = '1rem'
+        bottomDiv.style.alignItems = 'flex-start'
+        bottomDiv.style.height = '4rem'
+        bottomDiv.style.flexDirection = 'row'
+        divExpanded = false
+    }
+}
 
-writeLines(text)
+function clickHandler() {
+    if (divExpanded) toogleTextArea()
+    delta = 16 - document.querySelector('#GPU-slider').value
+    document.querySelector('#aiImg').style.display = 'none'
+    let centerDiv = document.querySelector('.centerdiv')
+    centerDiv.style.overflowY = 'auto'
+    centerDiv.style.display = 'block'
+    canvas.style.display = 'block'
+    let text = document.querySelector('#textInput').value
+    writeLines(text)
+}
+// text = ``
+
+// writeLines(text)
